bd7af3b20004efff267166b564e80defe4797a29
Derek Bender <170351+djbender@users.noreply.github.com>
2021-04-19 11:12:40 -0500

    fix combining logic

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 
 set -exu
 
-# no intermediate files
-jq --slurp '.[0] + .[1]' \
-  < <(curl --silent https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json) \
-  < <(curl --silent https://raw.githubusercontent.com/Qballjos/portainer_templates/master/Template/template.json) \
-  | tee >(jq) templates.json
+jq -s '{version: "2", templates: (.[0].templates + .[1].templates)}' < <(curl --silent https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json https://raw.githubusercontent.com/Qballjos/portainer_templates/master/Template/template.json)  \
+  | tee >(jq) > templates.json
